7651f4d927090d2b82244947ab45f7273e023a37
Evgeny Garlukovich <evgeny.garlukovich@gmail.com>
2017-07-25 22:33:34 +0300

    Apply refactoring to CalculateLongTermDepositSum using dependency injection

--- lib/calculate_long_term_deposit_sum.rb
+++ lib/calculate_long_term_deposit_sum.rb
@@ -1,9 +1,13 @@
 require 'calculate_annual_deposit_sum'
 
 class CalculateLongTermDepositSum
-  def call(interest_rate, annual_addition, years, annual_compounds_count)
-    calculate_annual_deposit_sum = CalculateAnnualDepositSum.new
+  attr_reader :calculate_annual_deposit_sum
 
+  def initialize(calculate_annual_deposit_sum = CalculateAnnualDepositSum.new)
+    @calculate_annual_deposit_sum = calculate_annual_deposit_sum
+  end
+
+  def call(interest_rate, annual_addition, years, annual_compounds_count)
     years.times.reduce(0) do |period_start_sum, _year|
       calculate_annual_deposit_sum.(interest_rate, (period_start_sum + annual_addition), annual_compounds_count)
     end
